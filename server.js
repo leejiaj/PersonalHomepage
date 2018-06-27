abc8dc0173a45cbc4d0ce24d34ba551ad465e122
Leejia James <leejiaj@gmail.com>
2018-06-26 22:09:12 -0500

    updates to deploy to heroku - not found error correction

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/PersonalHomepage'));
+app.use(express.static('/dist/jia999'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('/dist/PersonalHomepage/index.html'));
+res.sendFile(path.join('/dist/jia999/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
